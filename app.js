3bf2bbd3293eac43e003bc4d1f7e143383975d38
Justin Castilla <pxlperfection@gmail.com>
2015-10-13 11:32:20 -0700

    added andrew's additions to solution

--- app.js
+++ app.js
@@ -66,7 +66,42 @@ angular.module('GABook', [])
        image: 'https://avatars1.githubusercontent.com/u/3010270?',
        position: 'Instructor',
        cohort: 23,
-     }
+     },
+     {
+   name: 'Vikash Parekh',
+   github: 'vikashiscool',
+   image: 'https://avatars1.githubusercontent.com/u/9686256?v=3&s=460',
+   position: 'Developer In Residence',
+   cohort: 21,
+ },
+ {
+   name: 'Andrew Patzsch',
+   github: 'mrpatzsch',
+   image: 'https://avatars2.githubusercontent.com/u/10443141?v=3&s=460',
+   position: 'Developer In Residence',
+   cohort: 21,
+ },
+ {
+   name: 'Bedford Williamson',
+   github: 'goodbedford',
+   image: 'https://avatars1.githubusercontent.com/u/2454833?v=3&s=460',
+   position: 'Developer In Residence',
+   cohort: 22,
+ },
+ {
+   name: 'Ben Manning',
+   github: 'ben-manning',
+   image: 'https://avatars0.githubusercontent.com/u/8397980?v=3&s=460',
+   position: 'Instructor',
+   cohort: 24,
+ },
+ {
+   name: 'Travis Gaff',
+   github: 'tgaff',
+   image: 'https://avatars2.githubusercontent.com/u/1916144?v=3&s=460',
+   position: 'Developer In Residence',
+   cohort: 23,
+ }
     ];
     $scope.orderProperty = 'cohort';
   }]);
